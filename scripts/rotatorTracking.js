139ced51377f35b2a2295ebd362be9a53c73d1f1
karen-lim <karen.lim@sypherlabs.com>
2015-05-20 16:15:57 +0800

    Edited rotatorTracking.js

--- scripts/rotatorTracking.js
+++ scripts/rotatorTracking.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $(".dwuserEasyRotator erACR").click(function(){
+  $("#u36133").click(function(){
     ga('send', 'event', 'Rotator', 'Click', 'rotator');
   });
 });
